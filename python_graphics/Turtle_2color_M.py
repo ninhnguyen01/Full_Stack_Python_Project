52f8d7bf3364c35352e6ff9c0ea0ec0cf61d1eb6
Ninh <95522691+ninhnguyen01@users.noreply.github.com>
2025-11-02 16:49:15 -0800

    Add files via upload

---
+++ python_graphics/Turtle_2color_M.py
@@ -0,0 +1,22 @@
+# This turtle program draws a 2 color M. 
+
+import turtle
+turtle.showturtle()
+
+turtle.penup()
+turtle.isdown()
+turtle.goto(100,0)
+turtle.pendown()
+turtle.setheading(45)
+turtle.pencolor('blue')
+turtle.goto(150,50)
+turtle.setheading(120)
+turtle.goto(50,150)
+turtle.goto(0,120)
+turtle.pencolor('red')
+turtle.goto(-50,150)
+turtle.goto(-150,50)
+turtle.goto(-100,0)
+
+turtle.hideturtle()
+turtle.done()
---
+++ python_graphics/Turtle_3circles.py
@@ -0,0 +1,35 @@
+# This turtle program draws 3 circles on a grid.
+
+import turtle
+turtle.showturtle()
+turtle.heading()
+
+turtle.forward(250)
+turtle.setheading(180)
+turtle.forward(500)
+turtle.home()
+turtle.setheading(90)
+turtle.forward(250)
+turtle.setheading(270)
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(-60,0)
+turtle.pendown()
+turtle.pencolor('red')
+turtle.circle(60)
+
+turtle.penup()
+turtle.goto(-120,0)
+turtle.pendown()
+turtle.pencolor('green')
+turtle.circle(120)
+
+turtle.penup()
+turtle.goto(-180,0)
+turtle.pendown()
+turtle.pencolor('blue')
+turtle.circle(180)
+turtle.hideturtle()
+
+turtle.done()
