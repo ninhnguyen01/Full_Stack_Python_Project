a0a764df8e2d9bc44c9bf5b4195dc5b28a1fc9e7
Ninh <95522691+ninhnguyen01@users.noreply.github.com>
2025-11-25 13:49:23 -0800

    Add files via upload

---
+++ API_Dev/Color_Generator/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+import random
+from string import hexdigits
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    hex_chars = "".join(random.choices(hexdigits.lower(), k=6))
+    hex_color = f"#{hex_chars}"
+    context = {
+        "color": hex_color,
+    }
+    return templates.TemplateResponse(
+        request=request, name="color.html", context=context
+    )
