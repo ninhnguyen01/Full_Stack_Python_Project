46ea78f88f363b61250fd6c359372663365b23df
Ninh <95522691+ninhnguyen01@users.noreply.github.com>
2026-01-09 22:42:45 -0800

    Delete Python Mini-Program/dinner_cost.py

--- Python Mini-Program/dinner_cost.py
+++
@@ -1,9 +0,0 @@
-# Tip, Tax, and Total Program
-
-tip_percentage = 0.18
-sales_tax_percentage = 0.07
-meal_subtotal = float(input('Enter the total charge for food: '))
-meal_tip = meal_subtotal * tip_percentage
-meal_salestax= meal_subtotal * sales_tax_percentage
-meal_total = meal_subtotal + meal_tip + meal_salestax 
-print(meal_total)
