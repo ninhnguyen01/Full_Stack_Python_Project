ed857a4d52bc7ae850b7a3b5ebe0bbeff3c5aed7
Ninh <95522691+ninhnguyen01@users.noreply.github.com>
2026-01-09 22:40:48 -0800

    Delete Introduction to APIs in Python directory

--- Introduction to APIs in Python/format_text.py
+++
@@ -1,7 +0,0 @@
-import json
-
-album = {'id': 42, 'title':"Back in Black"}
-string = json.dumps(album) # object to string
-print(string)
-albumm = json.loads(string) # string to object
-print(album)
