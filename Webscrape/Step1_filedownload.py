2085b92e0d86eea5e4449f05b91071b16aa3d21e
Ninh <95522691+ninhnguyen01@users.noreply.github.com>
2026-01-09 22:44:26 -0800

    Delete Webscrape directory

--- Webscrape/Step1_filedownload.py
+++
@@ -1,7 +0,0 @@
-# import package
-from urllib.request import urlretrieve
-
-# retrieve file
-url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv'
-
-urlretrieve(url,'winequality-white.csv')
